75749e7abab398938a5f14b40bad42e3cc6b71e2
Emmanuel Dadzie <manueldadzie442@gmail.com>
2026-03-01 23:39:10 +0000

    Fix case-sensitive import paths for Vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Home from "./pages/home";
+import Login from "./pages/login";
 import AdminDashboard from "./pages/AdminDashboard";
 import StudentDashboard from "./pages/StudentDashboard";
 import AdminStudentView from "./pages/AdminStudentView";
